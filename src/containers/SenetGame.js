b6133deda0c3c77f2d5acb766b27a0b4d4c1b519
Catrin Hughes <catrinhughes92@gmail.com>
2020-03-06 10:49:04 +0000

    created SenetGame component

---
+++ src/containers/SenetGame.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class SenetGame extends Componenet {
+  constructor(props) {
+    super(props);
+    this.state = {
+      diceRoll: [],
+      whitePieces: [
+        { id: 1, position: null },
+        { id: 2, position: null },
+        { id: 3, position: null },
+        { id: 4, position: null },
+        { id: 5, position: null }
+      ],
+      blackPieces: [
+        { id: 1, position: null },
+        { id: 2, position: null },
+        { id: 3, position: null },
+        { id: 4, position: null },
+        { id: 5, position: null }
+      ],
+      winner: null
+    }
+  }
+}
+
+export default SenetGame;
