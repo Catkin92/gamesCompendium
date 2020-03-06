b66b9c4c3b94d7fc48250cd53974c24b36c7950d
Catrin Hughes <catrinhughes92@gmail.com>
2020-03-06 12:46:07 +0000

    reversed middle row of game board

--- src/components/SenetBoard.js
+++ src/components/SenetBoard.js
@@ -3,9 +3,11 @@ import SenetCell from './SenetCell';
 
 const SenetBoard = () => {
 
+  //  CREATES AN ARRAY OF 30 SENET CELLS
   function createBoard() {
     const gameBoard = [];
     let i;
+    // ALTERNATES BLACK AND WHITE COLOURS
     for (i = 0; i < 30; i++) {
       if (i % 2 === 0) {
         gameBoard.splice(i, 0, <SenetCell key={i} colour="whiteCell" id={i + 1}/>)
@@ -14,14 +16,22 @@ const SenetBoard = () => {
         gameBoard.splice(i, 0, <SenetCell key={i} colour="blackCell" id={i + 1}/>)
       }
     }
-    console.log(gameBoard);
-    return gameBoard;
+    // TAKES OFF THE LAST 10 CELLS AND SAVES TO ANOTHER ARRAY
+    const gameBoardEnd = gameBoard.slice(20);
+    gameBoard.splice(20, 10);
+
+    // RETURNS AN ARRAY WITH THE MIDDLE ROW (11-20) REVERSED
+    const newGameBoard = gameBoard.splice(0, 10)
+    .concat(gameBoard.reverse())
+    .concat(gameBoardEnd);
+    
+    return newGameBoard;
   }
 
   return (
     <>
     <h4>SenetBoard</h4>
-    <ul>{createBoard()}</ul>
+    <ul className="senetBoard">{createBoard()}</ul>
     </>
   )
 }
