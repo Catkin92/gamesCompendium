e6c6597c0d1833fef92a7a342bed917820570dc3
Catrin Hughes <catrinhughes92@gmail.com>
2020-03-11 11:55:20 +0000

    refactored SenetCell ternary

--- src/components/SenetCell.js
+++ src/components/SenetCell.js
@@ -2,15 +2,22 @@ import React from 'react';
 import SenetPiece from './SenetPiece';
 
 const SenetCell = ({ colour, id, piece, changePiecePosition }) => {
-// colour = colour of senet cell
-// id = id of senet cell
-// piece = id of senet piece
+  // colour = colour of senet cell
+  // id = id of senet cell
+  // piece = id of senet piece
 
   return (
-      <li className={colour}>
-        {id}
-       { piece ? <SenetPiece changePiecePosition={changePiecePosition} currentPosition={id} id={piece} colour={piece < 6 ? "whitePieces" : "blackPieces"}/> : null }
-      </li>
+    <li className={colour}>
+      {id}
+      {piece ? (
+        <SenetPiece
+          changePiecePosition={changePiecePosition}
+          currentPosition={id}
+          id={piece}
+          colour={piece < 6 ? "whitePieces" : "blackPieces"}
+        />) : (null)
+      }
+    </li>
   )
 }
 
